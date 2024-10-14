58c7c91a1c9c85c76030d23f43ad6cd5c141e2f0
HuzaifaAkhtar2 <huzaifaakh@outlook.com>
2024-10-14 18:07:31 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.title = "Rainbow using DOM"
+document.body.childNodes[1].children[0].style.backgroundColor = "red";
+document.body.childNodes[1].children[1].style.backgroundColor = "orange";
+document.body.childNodes[1].children[2].style.backgroundColor = "yellow";
+document.body.childNodes[1].children[3].style.backgroundColor = "green";
+document.body.childNodes[1].children[4].style.backgroundColor = "blue";
+document.body.childNodes[1].children[5].style.backgroundColor = "indigo";
+document.body.childNodes[1].children[6].style.backgroundColor = "violet";
